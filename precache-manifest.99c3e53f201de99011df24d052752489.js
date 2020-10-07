ce9f41be8ffb055c031a6c807c25cb2ffda83e78
Yelena Strizhakova <e.strizhakova@gmail.com>
2020-10-07 22:26:45 +0300

    Updates

---
+++ precache-manifest.99c3e53f201de99011df24d052752489.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "831ce23c2dca4b8ea176042cf4c336a4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "130b903eb7f98fff86f2",
+    "url": "./static/css/main.10a2bcb1.chunk.css"
+  },
+  {
+    "revision": "a1849692ce12fc74469f",
+    "url": "./static/js/2.afc66176.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.afc66176.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "130b903eb7f98fff86f2",
+    "url": "./static/js/main.091e5e6e.chunk.js"
+  },
+  {
+    "revision": "a8ef13021948c1ee7e9c",
+    "url": "./static/js/runtime-main.a5163ae5.js"
+  },
+  {
+    "revision": "d476a8b86d0b656cfa463c1148e96069",
+    "url": "./static/media/avatar-edit-button.d476a8b8.svg"
+  },
+  {
+    "revision": "5df32a06fdf69f3ae19600017c36d45a",
+    "url": "./static/media/close-icon.5df32a06.svg"
+  },
+  {
+    "revision": "6bfdb35467f51d3ba1f70f8d4a9c2b59",
+    "url": "./static/media/edit-btn.6bfdb354.svg"
+  },
+  {
+    "revision": "5bbb05f725aa334a73acf3174e255f4e",
+    "url": "./static/media/element-like-active.5bbb05f7.svg"
+  },
+  {
+    "revision": "c6f110191388042ab958ac5e9b285425",
+    "url": "./static/media/logo.c6f11019.svg"
+  }
+]);
